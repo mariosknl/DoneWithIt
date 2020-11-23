17c8517694e7a3c7ae42fe2bb236d10d74afb7a9
mariosknl <mariosknl@outlook.com>
2020-11-23 17:45:39 +0200

    refreshing list

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -9,6 +9,7 @@ function Screen({ children }) {
 const styles = StyleSheet.create({
   screen: {
     paddingTop: Constants.statusBarHeight,
+    flex: 1,
   },
 });
 
--- app/screen/MessagesScreen.js
+++ app/screen/MessagesScreen.js
@@ -23,6 +23,7 @@ const initialMessages = [
 
 function MessagesScreen(props) {
   const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
 
   const handleDelete = (message) => {
     // delete the message from messages
@@ -46,6 +47,17 @@ function MessagesScreen(props) {
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/guy.jpg"),
+            },
+          ]);
+        }}
       />
     </Screen>
   );
