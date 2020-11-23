fdadabea9d48c8588ae7ded277c46474be5eaebc
mariosknl <mariosknl@outlook.com>
2020-11-23 17:59:20 +0200

    extending the listItem component

--- App.js
+++ App.js
@@ -9,11 +9,12 @@ import Card from "./app/components/Card";
 import MessagesScreen from "./app/screen/MessagesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
   return (
     <Screen>
-      <Icon name="email" size={100} />
+      <ListItem ImageComponent={<Icon name="email" />} title="My title" />
     </Screen>
   );
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -5,15 +5,23 @@ import AppText from "./AppText";
 
 import colors from "../config/colors";
 
-function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
+function ListItem({
+  title,
+  subTitle,
+  image,
+  ImageComponent,
+  onPress,
+  renderRightActions,
+}) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
         <View style={styles.container}>
-          <Image style={styles.image} source={image} />
-          <View style={styles}>
+          {ImageComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -26,11 +34,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     padding: 15,
   },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
   },
   subTitle: {
     color: colors.medium,
