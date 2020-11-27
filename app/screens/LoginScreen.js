fc00e8882bdd14ffe8c9bc6f101bd2a3ed4502a7
mariosknl <mariosknl@outlook.com>
2020-11-27 19:36:24 +0200

    jwt decode implemented

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, Image } from "react-native";
 import * as Yup from "yup";
+import jwtDecode from "jwt-decode";
 
 import Screen from "../components/Screen";
 import {
@@ -23,7 +24,8 @@ function LoginScreen(props) {
     const result = await authApi.login(email, password);
     if (!result.ok) return setLoginFailed(true);
     setLoginFailed(false);
-    console.log(result.data);
+    const user = jwtDecode(result.data);
+    console.log(user);
   };
 
   return (
