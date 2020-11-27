e312c03af345b37e9e5df9a48c45ee7e0011e3b4
mariosknl <mariosknl@outlook.com>
2020-11-27 17:19:57 +0200

    reset form values after making the post request

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -85,7 +85,7 @@ function ListingEditScreen() {
   const [uploadVisible, setUploadVisible] = useState(false);
   const [progress, setProgress] = useState(0);
 
-  const handleSubmit = async (listing) => {
+  const handleSubmit = async (listing, { resetForm }) => {
     setProgress(0);
     setUploadVisible(true);
     const result = await listingsApi.addListing(
@@ -97,6 +97,8 @@ function ListingEditScreen() {
       setUploadVisible(false);
       return alert("Could not save the listing");
     }
+
+    resetForm();
   };
 
   return (
