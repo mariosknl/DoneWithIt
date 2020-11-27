54180f659aa6e69f84f4b410763d6d0ad0524e17
mariosknl <mariosknl@outlook.com>
2020-11-27 19:00:55 +0200

    auth implemented

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import OfflineNotice from "./app/components/OfflineNotice";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        <AuthNavigator />
       </NavigationContainer>
     </>
   );
