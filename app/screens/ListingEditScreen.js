27d794c51e2f042ce65be112823d3844c15d1ef4
mariosknl <mariosknl@outlook.com>
2020-11-27 17:14:37 +0200

    showing confirmation after uploading the image

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -92,15 +92,20 @@ function ListingEditScreen() {
       { ...listing, location },
       (progress) => setProgress(progress)
     );
-    setUploadVisible(false);
 
-    if (!result.ok) return alert("Could not save the listing");
-    alert("Success");
+    if (!result.ok) {
+      setUploadVisible(false);
+      return alert("Could not save the listing");
+    }
   };
 
   return (
     <Screen style={styles.container}>
-      <UploadScreen progress={progress} visible={uploadVisible} />
+      <UploadScreen
+        onDone={() => setUploadVisible(false)}
+        progress={progress}
+        visible={uploadVisible}
+      />
       <Form
         initialValues={{
           title: "",
--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -2,21 +2,39 @@ import React from "react";
 import { Modal } from "react-native";
 import { View, StyleSheet } from "react-native";
 import * as Progress from "react-native-progress";
+import LottieView from "lottie-react-native";
 
 import Text from "../components/Text";
 import colors from "../config/colors";
 
-function UploadScreen({ progress = 0, visible = false }) {
+function UploadScreen({ onDone, progress = 0, visible = false }) {
   return (
     <Modal visible={visible}>
       <View style={styles.container}>
-        <Progress.Bar color={colors.primary} progress={progress} width={200} />
+        {progress < 1 ? (
+          <Progress.Bar
+            color={colors.primary}
+            progress={progress}
+            width={200}
+          />
+        ) : (
+          <LottieView
+            autoPlay
+            loop={false}
+            onAnimationFinish={onDone}
+            source={require("../assets/animations/done.json")}
+            style={styles.animation}
+          />
+        )}
       </View>
     </Modal>
   );
 }
 
 const styles = StyleSheet.create({
+  animation: {
+    width: 150,
+  },
   container: {
     alignItems: "center",
     flex: 1,
