237013d55d961dfe7bf3879797b6db1822136064
mariosknl <mariosknl@outlook.com>
2020-11-28 20:32:27 +0200

    settings function created for different environments

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,10 @@
 import { create } from "apisauce";
 import cache from "../utility/cache";
 import authStorage from "../auth/storage";
+import settings from "../config/settings";
 
 const apiClient = create({
-  baseURL: "http://192.168.11.93:9000/api",
+  baseURL: settings.apiUrl,
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
