9fb79ed74a0944c3a87fd966a29b790ff837fc24
mariosknl <mariosknl@outlook.com>
2020-11-28 11:57:40 +0200

    refactoring app component

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import jwtDecode from "jwt-decode";
 import { AppLoading } from "expo";
 
 import navigationTheme from "./app/navigation/navigationTheme";
@@ -14,15 +13,14 @@ export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-    setUser(jwtDecode(token));
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   };
 
   if (!isReady)
     return (
-      <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
     );
 
   return (
--- app/auth/storage.js
+++ app/auth/storage.js
@@ -1,4 +1,5 @@
 import * as SecureStore from "expo-secure-store";
+import jwtDecode from "jwt-decode";
 
 const key = "authToken";
 
@@ -18,6 +19,11 @@ const getToken = async () => {
   }
 };
 
+const getUser = async () => {
+  const token = await getToken();
+  return token ? jwtDecode(token) : null;
+};
+
 const removeToken = async () => {
   try {
     await SecureStore.deleteItemAsync(key);
@@ -26,4 +32,4 @@ const removeToken = async () => {
   }
 };
 
-export default { getToken, removeToken, storeToken };
+export default { getUser, removeToken, storeToken };
